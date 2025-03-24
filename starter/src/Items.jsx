20a48b543e2df76bef88c184a622e82b612bc45a
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-24 15:51:07 +0100

    update dependencies and refactor components to integrate react-query for data fetching

--- starter/src/Items.jsx
+++ starter/src/Items.jsx
@@ -1,8 +1,27 @@
-import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import SingleItem from "./SingleItem";
+import { useQuery } from "@tanstack/react-query";
+import customFetch from "./utils";
+
+const Items = () => {
+  const result = useQuery({
+    queryKey: ["tasks"],
+    queryFn: async () => {
+      const response = await customFetch.get("/");
+      return response.data;
+    },
+  });
+
+  const { isLoading, isError, error, data } = result;
+
+  if (isLoading) {
+    return <p style={{ marginTop: "1rem" }}>Loading...</p>;
+  }
+
+  console.log(result);
+
   return (
-    <div className='items'>
-      {items.map((item) => {
+    <div className="items">
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
